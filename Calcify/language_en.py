e101de5d067ad7c62421a6d64c84410e73de8340
Kristof Toth <titanicbobo@gmail.com>
2022-07-22 15:38:41 +0200

    Python migration started. Complex calculation features removed. Conversion feature at WIP.

---
+++ Calcify/language_en.py
@@ -0,0 +1,49 @@
+# Headers
+mainh="CALCIFY"
+calch="CALCULATE"
+basich="BASIC CALCULATION"
+scienth="SCIENTIFIC CALCULATION"
+credh="CREDITS"
+convh="CONVERT"
+lengh="LENGTH"
+massh="MASS"
+complexh="COMPLEX"
+techh="TECHNICAL"
+elech="ELECTRICAL"
+phyh="PHYSICAL"
+stath="STATISTICS"
+
+# Info
+infocalc="\n\n To calculate, enter a number first. \n Then enter the operation from the list. \n After that, enter the second number."
+calcmenu_sci="\n\n [+] Addition\n [-] Subtraction\n [*] Multiplication\n [/] Division\n [R] Square Root\n [2] Power by two\n [n] Power by n\n [s] Sin\n [c] Cos\n [t] Tan\n [S] arcsin\n [C] arcos\n [T] arctan"
+calcmenu_bsc="\n\n [+] Addition\n [-] Subtraction\n [*] Multiplication\n [/] Division"
+mainmenu="\n\n [1] Calculator\n [2] Converter\n [3] Credits\n [4] Exit"
+# mainmenu="\n [1] Calculator\n [2] Converter\n [3] Statistics\n [4] Credits\n [5] Exit\n"
+mainmenu_calc="[1] Basic\n [2] Scientific\n [3] Complex\n [4] Unit Calculation"
+mainmenu_conv="[1] Length\n [2] Mass"
+techmenu="\n [1] Physical\n [2] Electrical"
+techphy_menu="\n [1] Velocity [v]\n [2] Acceleration [a]\n [3] Power [P]\n [4] Force [F]\n [5] Momentum [p]\n [6] Work [W]\n [7] Kinetic Energy [E kin]\n [8] Potential Energy [E pot]\n [9] Pressure [p]"
+techelec_menu="\n [1] Voltage [U]\n [2] Electric current [I]\n [3] Electrical Resistance [R]\n [4] Electrical Impendance [Z]\n [5] Capacitive Resistance [XC]\n [6] Inductive Resistance [XL]\n [7] Magnetic flux [B]\n [8] Electric Charge [Q]\n [9] Electric Field [E]\n [10] Capacity [C]"
+
+# Inputs
+mode="Mode:"
+category="Category:"
+sqrroot="Sqr. Root of "
+frm="\n From: \n"
+to="\n To: \n"
+enter_num="\n Enter a number: "
+enter_real="\n Enter a Real number: "
+enter_imag=" Enter a Imaginary number: "
+
+# Questions
+ask_to_add=" Add another complex number?\n [Y] Yes\n [N] No\n"
+
+# Outputs
+add="Addition: "
+sub="Subtraction: "
+mul="Multiplication: "
+divs="Division: "
+error1="ERROR: there is no"
+error2="command"
+
+enter="\n Press ENTER to continue..."
---
+++ Calcify/language_hu.py
@@ -0,0 +1,49 @@
+# Headers
+mainh="CALCIFY"
+calch="SZÁMOLÁS"
+basich="ALAP SZÁMOLÁS"
+scienth="TUDOMÁNYOS SZÁMOLÁS"
+credh="NÉVJEGY"
+convh="ÁTALAKÍTÁS"
+lengh="HOSSZ"
+massh="TÖMEG"
+complexh="KOMPLEX"
+techh="MŰSZAKI"
+elech="ELEKTRONIKAI"
+phyh="FIZIKAI"
+stath="STATISZIKA"
+
+#Info
+infocalc="\n\n A számoláshoz adj meg egy számot. \n Aztán válassz egy műveletet a listáról. \n Ez után adj meg egy második számot."
+calcmenu_sci="\n\n [+] Hozzáadás\n [-] Kivonás\n [*] Szorzás\n [/] Osztás\n [R] Gyökvonás\n [2] a négyzeten\n [n] az n-ediken\n [s] szinusz\n [c] koszinusz\n [t] tangens\n [S] arkuszszinusz\n [C] arkuszkoszinusz\n [T] arkusztangens"
+calcmenu_bsc="\n\n [+] Hozzáadás\n [-] Kivonás\n [*] Szorzás\n [/] Osztás"
+mainmenu="\n\n [1] Számológép\n [2] Átvaltó\n [3] Névjegy\n [4] Kilépés"
+#mainmenu="\n [1] Calculator\n [2] Converter\n [3] Statistics\n [4] Credits\n [5] Exit\n"
+mainmenu_calc="[1] Alap\n [2] Tudományos\n [3] Komplex\n [4] Mértékegység számolás"
+mainmenu_conv="[1] hossz\n [2] tömeg"
+techmenu="\n [1] fizikai\n [2] elektronikai"
+techphy_menu="\n [1] sebesség [v]\n [2] gyorsulás [a]\n [3] teljesítmény [P]\n [4] erő [F]\n [5] lendület [p]\n [6] munka [W]\n [7] mozgási energia [E kin]\n [8] helyzeti energia [E pot]\n [9] nyomás [p]"
+techelec_menu="\n [1] feszültség [U]\n [2] elektromos áram [I]\n [3] elektromos ellenállás [R]\n [4] elektromos impedancia [Z]\n [5] kapacitív ellenállás [XC]\n [6] induktív ellenállás [XL]\n [7] mágneses fluxus [B]\n [8] elektromos töltés [Q]\n [9] elektromos mező [E]\n [10] kapacitás [C]"
+
+#Inputs
+mode="Mód:"
+category="Kategória:"
+sqrroot="Négyzetgyöke ennek"
+frm="\n Erről: \n"
+to="\n Erre: \n"
+enter_num="\n Adj meg egy számot: "
+enter_real="\n Adj meg egy valós számot: "
+enter_imag=" Adj meg egy képzeletbeli számot: "
+
+#Questions
+ask_to_add=" Hozzáadjunk még egy komplex számot?\n [Y] Igen\n [N] Nem\n"
+
+#Outputs
+add="Hozzáadás:"
+sub="Kivonás:"
+mul="Szorzás:"
+divs="Osztás:"
+error1="HIBA: nincs"
+error2="nevű parancs"
+
+enter="\n Nyomd meg az ENTER-t a folytatáshoz..."
